974825b06d7cc6b0ef198a53f623896660e016bc
Stepan Suvorov <stevermeister@gmail.com>
2015-11-06 08:37:08 +0100

    Update functions in functions.js
    that's what I mean when said to separate function declarations and switch

--- functions in functions.js
+++ functions in functions.js
@@ -1,47 +1,37 @@
 var calcLog = [];
 
 function calcFactory(symbol) {
-    switch (symbol){
-        case '+':
-        function sum(a,b) {
+    
+    function sum(a,b) {
             var c = a + b;
             calcLog.push(a.toString()+ symbol + b.toString() + '=' + c.toString());
             return c
         }
-            return sum;
-            break;
-
-        case '*':
-        function multiply(a,b) {
+    function multiply(a,b) {
             var c = a * b;
             calcLog.push(a.toString()+ symbol + b.toString() + '=' + c.toString());
             return c
         }
-            return multiply;
-            break;
-
-        case '^':
-        function power(a,b) {
+    function power(a,b) {
             var c = Math.pow(a,b);
             calcLog.push(a.toString()+ symbol + b.toString() + '=' + c.toString());
             return c
-        }
-            return power;
-            break;
-
-        case '/':
-        function divide(a,b) {
+        }    
+    function divide(a,b) {
             var c = a / b;
             calcLog.push(a.toString()+ symbol + b.toString() + '=' + c.toString());
             return c
         }
-            return divide;
-            break;
-
-        default:
-        function unknown(a,b){
+    function unknown(a,b){
             return "Sorry, we don't get your calculation. Please try again."
-        }
-            return unknown
+        }   
+        
+        
+    switch (symbol){
+        case '+': return sum; break;
+        case '*': return multiply;break;
+        case '^': return power; break;
+        case '/': return divide; break;
+        default:return unknown
     }
 }
