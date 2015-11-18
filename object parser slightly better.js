6ee36a3707f21eb53457606d6b6c55800869a021
Gerhard Riphagen <gerhard@studytube.nl>
2015-11-18 11:29:32 +0100

    created counter object to store results, created recursive function to deal with local variables

--- object parser slightly better.js
+++ object parser slightly better.js
@@ -1,76 +1,79 @@
 var obj = {
     a: 1,
     b: 'string',
-    c: 2,
+    c: [1,2,'wwer',4],
     d: true,
     e: {
         f: 1,
-        g: 'fsdfs'
-    }
-};
-
-var objectParser = function(obj) {
-
-    if (typeof strings === 'undefined') strings = 0;
-    if (typeof numbers === 'undefined') numbers = 0;
-    if (typeof booleans === 'undefined') booleans = 0;
-    if (typeof objects === 'undefined') objects = 0;
-    if (typeof functions === 'undefined') functions = 0;
-    if (typeof arrays === 'undefined') arrays = 0;
-    if (typeof dates === 'undefined') dates = 0;
-
-
-    for (var prop in obj) {
-        if (obj.hasOwnProperty(prop)) {
-
-            if(typeof obj[prop] === 'string'){
-                ++strings;
-            }
-
-            if(typeof obj[prop] === 'number'){
-                ++numbers;
-            }
-
-            if(typeof obj[prop] === 'boolean'){
-                ++booleans;
-            }
-
-            if(typeof obj[prop] === 'object' && !Array.isArray(obj[prop]) && !(obj[prop] instanceof Date)){
-                ++objects;
-                objectParser(obj[prop]);
-            }
-
-            if(typeof obj[prop] === 'function'){
-                ++functions;
-            }
-
-            if(typeof obj[prop] === 'object' && Array.isArray(obj[prop])){
-                ++arrays;
-                objectParser(obj[prop]);
-            }
-
-            if(obj[prop] instanceof Date){
-                ++dates;
+        g: 'fsdfs',
+        h: [1,2,3],
+        i: {
+            j: 1,
+            k: "1212313",
+            z: {
+                x: 'afdfsdasadf',
+                y: new Date(11,11,11),
+                u: function(){
+                    console.log('function')
+                }
             }
         }
     }
-
-
-    var types = {
-        'strings': strings,
-        'numbers': numbers,
-        'booleans': booleans,
-        'objects': objects,
-        'functions': functions,
-        'arrays': arrays,
-        'dates': dates
-    };
-
-    return types;
 };
 
 
+var objectParser = function(obj) {
+
+    var counter = {
+        stringsNumber: 0,
+        numbersNumber: 0,
+        booleansNumber: 0,
+        objectsNumber: 0,
+        functionsNumber: 0,
+        arraysNumber: 0,
+        datesNumber: 0
+    };
+
+    recursiveObject = function(obj) {
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+
+                if(typeof obj[prop] === 'string'){
+                    ++counter.stringsNumber;
+                }
+
+                if(typeof obj[prop] === 'number'){
+                    ++counter.numbersNumber;
+                }
+
+                if(typeof obj[prop] === 'boolean'){
+                    ++counter.booleansNumber;
+                }
+
+                if(typeof obj[prop] === 'object' && !Array.isArray(obj[prop]) && !(obj[prop] instanceof Date)){
+                    ++counter.objectsNumber;
+                    recursiveObject(obj[prop]);
+                }
+
+                if(typeof obj[prop] === 'function'){
+                    ++counter.functionsNumber;
+                }
+
+                if(Array.isArray(obj[prop])){
+                    ++counter.arraysNumber;
+                    recursiveObject(obj[prop]);
+                }
+
+                if(obj[prop] instanceof Date){
+                    ++counter.datesNumber;
+                }
+            }
+        }
+    };
+
+    recursiveObject(obj);
+
+    return counter;
+};
+
 objectParser(obj);
-
-
-
